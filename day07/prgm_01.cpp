f2bf6a3d23263c75d361a29f44a9830df1aa149f
SreelakshmiS123 <94931897+SreelakshmiS123@users.noreply.github.com>
2025-07-15 16:40:37 +0530

    Update prgm_01.cpp

--- day07/prgm_01.cpp
+++ day07/prgm_01.cpp
@@ -1,3 +1,4 @@
+//bootloader.asm
 [ORG 0x7C00]
 MOV AH, 0x0E
 MOV AL, 'H'
@@ -10,3 +11,16 @@ TIMES 510 - ($ - $$) DB 0
 DW 0xAA55
 
 /c/msys64/mingw64/bin/qemu-system-x86_64 -drive format=raw,file=bootloader.bin
+
+  //kernel.cpp
+  extern "C" void kernel_main() {
+    const char* message = "Hello from kernel!";
+    char* video_memory = (char*)0xb8000;
+
+    for (int i = 0; message[i] != '\0'; ++i) {
+        video_memory[i * 2] = message[i];      // Character
+        video_memory[i * 2 + 1] = 0x07;        // Attribute byte: light grey on black
+    }
+
+    while (1); // Infinite loop to keep kernel running
+}
